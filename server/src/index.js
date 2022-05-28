f2f371a854ea0887f617e5e39a1006e3a88b8031
Nga Pham <ngapham3108@gmail.com>
2022-05-28 05:13:00 -0700

    Edit to deploy

--- server/src/index.js
+++ server/src/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const authRouter = require('./routes/auth');
 const postRouter = require('./routes/posts');
